63252ff0bd66acf3a29f6d4f2cf1dabeb351a186
Clare Jolly <clarejollyuk@gmail.com>
2018-10-22 23:46:57 +0100

    starting comments

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -4,5 +4,5 @@ def setup_test_database
   p 'setting up test databases'
   connection = PG.connect(dbname: "bookmark_manager_test")
 
-  connection.exec("TRUNCATE bookmarks;")
+  connection.exec("TRUNCATE bookmarks, comments;")
 end
